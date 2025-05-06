b6a394eceef7328be0b052a5e1e84ffe63fcf010
José J. Peleato Pradel <jjpeleato@gmail.com>
2025-05-06 20:26:53 +0200

    feat(source): save data when the form is submitted

--- ps_admin_panel_legacy.php
+++ ps_admin_panel_legacy.php
@@ -47,6 +47,12 @@ use PrestaShop\PrestaShop\Core\Module\WidgetInterface;
 //phpcs:ignore
 class Ps_Admin_Panel_Legacy extends Module implements WidgetInterface
 {
+    /** @var array $shops */
+    private array $shops = [];
+
+    /** @var array $languages */
+    private array $languages = [];
+
     /** @var string $domain */
     private string $domain = 'Modules.Psadminpanellegacy.Admin';
 
@@ -80,6 +86,9 @@ class Ps_Admin_Panel_Legacy extends Module implements WidgetInterface
             $this->domain
         );
         $this->confirmUninstall = $this->trans('Are you sure you want to uninstall?', [], $this->domain);
+
+        $this->shops = Shop::getShops();
+        $this->languages = Language::getLanguages(false);
 
         parent::__construct();
     }
@@ -115,14 +124,11 @@ class Ps_Admin_Panel_Legacy extends Module implements WidgetInterface
      */
     private function installShopFixtures(): bool
     {
-        $shops = Shop::getShops();
-        $languages = Language::getLanguages(false);
-
-        foreach ($shops as $shop) {
+        foreach ($this->shops as $shop) {
             $idShopGroup = (int) $shop['id_shop_group'];
             $idShop = (int) $shop['id_shop'];
 
-            foreach ($languages as $lang) {
+            foreach ($this->languages as $lang) {
                 $idLang = (int) $lang['id_lang'];
 
                 if (!$this->installLanguageFixture($idLang, $idShopGroup, $idShop)) {
@@ -181,6 +187,205 @@ class Ps_Admin_Panel_Legacy extends Module implements WidgetInterface
         PrestaShopLogger::addLog("Uninstalled module: $this->name", 1);
 
         return parent::uninstall();
+    }
+
+    /**
+     * This method is used to get the content of the module.
+     * It is called when the module is displayed in the back office.
+     */
+    public function getContent()
+    {
+        /**
+         * If the multi-store is active, check if the store is selected
+         * and display an error message if not.
+         */
+        $isMultistore = Shop::isFeatureActive();
+        if ($isMultistore === true) {
+            $idShop = (int) Shop::getContextShopID();
+
+            if ($idShop === 0) {
+                return $this->displayInformation(
+                    $this->trans('You must select a store.', [], $this->domain)
+                );
+            }
+        }
+
+        /**
+         * If submitted, the save process is triggered.
+         */
+        $output = '';
+        if (Tools::isSubmit('submit_' . $this->name)) {
+            $output = $this->postProcess();
+        }
+
+        /**
+         * Render all templates.
+         */
+        $renderTemplate = $this->context->smarty->fetch($this->local_path . 'views/templates/admin/index.tpl');
+
+        return $output . $this->renderForm() . $renderTemplate;
+    }
+
+    /**
+     * This method is used to render the form.
+     * It is called when the module is displayed in the back office.
+     *
+     * @see https://devdocs.prestashop-project.org/8/development/components/helpers/helperform/#attributes
+     *
+     * @return string
+     */
+    private function renderForm()
+    {
+        $helper = new HelperForm();
+
+        // Module, table, name_controller, token and currentIndex.
+        $helper->module = $this;
+        $helper->table = $this->table;
+        $helper->name_controller = $this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+        $helper->currentIndex = AdminController::$currentIndex . '&' . http_build_query(['configure' => $this->name]);
+
+        // Default language and languages.
+        $helper->default_form_language = (int) Configuration::get('PS_LANG_DEFAULT');
+        $helper->languages = $this->context->controller->getLanguages();
+
+        // Submit and identifier.
+        $helper->identifier = $this->identifier;
+        $helper->submit_action = 'submit_' . $this->name;
+
+        // Load current value into the form.
+        $helper->fields_value = $this->getConfigFieldsValues();
+
+        return $helper->generateForm([$this->getConfigForm()]);
+    }
+
+    /**
+     * Get configuration form.
+     *
+     * @see https://devdocs.prestashop-project.org/8/development/components/helpers/helperform/#basic-usage
+     *
+     * @return array
+     */
+    private function getConfigForm(): array
+    {
+        return [
+            'form' => [
+                'tinymce' => true,
+                'legend' => [
+                    'title' => $this->trans('Settings', [], $this->domain),
+                    'icon' => 'icon-cogs'
+                ],
+                'input' => [
+                    'title' => [
+                        'type' => 'text',
+                        'lang' => true,
+                        'label' => $this->trans('Title', [], $this->domain),
+                        'name' => 'PS_ADMIN_PANEL_LEGACY_TITLE',
+                        'desc' => $this->trans('Lorem ipsum dolor sit amet.', [], $this->domain)
+                    ],
+                    'short_description' => [
+                        'type' => 'textarea',
+                        'lang' => true,
+                        'label' => $this->trans('Short description', [], $this->domain),
+                        'name' => 'PS_ADMIN_PANEL_LEGACY_SHORT_DESCRIPTION',
+                        'cols' => 40,
+                        'rows' => 40,
+                        'class' => 'rte',
+                        'autoload_rte' => true,
+                    ],
+                    'description' => [
+                        'type' => 'textarea',
+                        'lang' => true,
+                        'label' => $this->trans('Description', [], $this->domain),
+                        'name' => 'PS_ADMIN_PANEL_LEGACY_DESCRIPTION',
+                        'cols' => 40,
+                        'rows' => 40,
+                        'class' => 'rte',
+                        'autoload_rte' => true,
+                    ],
+                ],
+                'submit' => [
+                    'title' => $this->trans('Save', [], $this->domain),
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Get configuration fields values.
+     *
+     * @return array
+     */
+    private function getConfigFieldsValues(): array
+    {
+        $fields = [];
+
+        foreach ($this->languages as $lang) {
+            $idLang = (int) $lang['id_lang'];
+
+            foreach ($this->fields as $key => $value) {
+                $fields[$key][$idLang] = Tools::getValue(
+                    $key . '_' . $idLang,
+                    Configuration::get($key, $idLang)
+                );
+            }
+        }
+
+        $fields['is_default'] = Tools::getValue('is_default', 0);
+
+        return $fields;
+    }
+
+    /**
+     * This method is used to process the form submission.
+     * It is called when the form is submitted.
+     *
+     * @return string
+     */
+    private function postProcess(): string
+    {
+        $errors = [];
+
+        foreach ($this->fields as $key => $value) {
+            $data = [];
+
+            foreach ($this->languages as $lang) {
+                $idLang = (int) $lang['id_lang'];
+                $data[$key][$idLang] = Tools::getValue($key . '_' . $idLang);
+            }
+
+            // Update the configuration value for each language.
+            $success = Configuration::updateValue(
+                $key,
+                $data,
+                true
+            );
+
+            // If the update fails, add an error message.
+            if (!$success) {
+                $errors[] = $this->trans(
+                    'Failed to save the value for "%key%" in language ID %idLang%.',
+                    [
+                        '%key%' => $key,
+                        '%idLang%' => $idLang
+                    ],
+                    $this->domain
+                );
+            }
+        }
+
+        // Clear the cache after updating the configuration.
+        $this->_clearCache('*');
+
+        // If there are errors, display them.
+        if (!empty($errors)) {
+            return $this->displayError(implode('<br>', $errors));
+        }
+
+        // If everything is successful, display a success message.
+        return $this->displayConfirmation(
+            $this->trans('The settings have been updated.', [], $this->domain)
+        );
     }
 
     /**
